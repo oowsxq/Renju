87b220553788342dfa5d8322c1a093577c501824
oowsxq <oowsxq@user.noreply.github.com>
2019-10-25 20:58:17 +0800

    accomplish opening library

--- src/com/engine/fourtransEngine/OpenLib.java
+++ src/com/engine/fourtransEngine/OpenLib.java
@@ -1,7 +1,5 @@
 package com.engine.fourtransEngine;
 
-import jdk.nashorn.internal.runtime.regexp.joni.constants.OPCode;
-
 import java.awt.*;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -127,52 +125,145 @@ public class OpenLib {
 
     /**
      * 按照开局名称返回开局模式
-     * @param hashkey
+     * @param name
      * @return null 则开局库中无此开局
      */
-    public static OpenPattern getPatternByName(long hashkey){
+    public static OpenPattern getPatternByName(String name){
         initOpenPattern();
         OpenPattern result = null;
         Iterator<Map.Entry<Long, OpenPattern>> iter = lib.entrySet().iterator();
         while (iter.hasNext()){
             Map.Entry<Long, OpenPattern> element = iter.next();
-            if (element.getKey() == hashkey)
+            if (element.getValue().name.equals(name)) {
                 result = element.getValue();
+                break;
+            }
         }
         return result;
     }
 
+    /**
+     * 随机获取一个平衡开局 { 3D, 11D }
+     * @return
+     */
+    public static OpenPattern getRandomPatternEqual(){
+        if (Math.random() < 0.5)
+            return getPatternByName("3D");
+        else
+            return getPatternByName("11D");
+    }
+
+//    /**
+//     * 随机获取一个黑方绝对优势的开局 { 7I, 4D }
+//     * @return
+//     */
+//    public static OpenPattern getRandomPatternBlackSureWin(){
+//
+//    }
+
     private static void utilInsertDirectPattern(String name, Point step3, double strength){
-        Board board_tmp = new Board(basicDirectBoard);
-        Long hashkey_tmp = null;
-        Point[] steps = null;
+//        Board board_tmp = null; //暂时用不上，用null插入，以后有需要再改
+        Point[] steps = new Point[3];
+        steps[0] = new Point(7,7);
+        steps[1] = new Point(6,7);
+        steps[2] = new Point(step3);
+        long hashkey = Zobrist.getRebuildedZobrist(steps).getCurrentCode();
 
         //插入模式
+        utilInsertMutiple(name, null, hashkey, steps, strength);
 
-        //插入对称的等效模式
     }
 
     private static void utilInsertInclinedPattern(String name, Point step3, double strength){
-        Board board_tmp = new Board(basicInclinedBoard);
-        Long hashkey_tmp = null;
-        Point[] steps = null;
+//        Board board_tmp = null; //暂时用不上，用null插入，以后有需要再改
+        Point[] steps = new Point[3];
+        steps[0] = new Point(7,7);
+        steps[1] = new Point(6,8);
+        steps[2] = new Point(step3);
+        long hashkey = Zobrist.getRebuildedZobrist(steps).getCurrentCode();
+
+        //插入模式
+        utilInsertMutiple(name, null, hashkey, steps, strength);
     }
 
-    class OpenPattern{
-        public String name;             //局面名称
-        public Board board;             //棋盘局面
-        public long hashkey;            //哈希值
-        public Point[] steps;            //三步的步法
-        public double strength;          //强度 0 ~ 1，越小对黑越有利
-        OpenPattern(String name, Board board, long hashkey, Point[] steps, double strength){
-            this.name = name;
-            this.board = new Board(board);
-            this.hashkey = hashkey;
-            this.steps = new Point[3];
-            steps[0] = new Point(steps[0]);
-            steps[1] = new Point(steps[1]);
-            steps[2] = new Point(steps[2]);
-            this.strength = strength;
+    /**
+     * 向开局库中写入一种开局及其他7种等效排布
+     * @param name
+     * @param board
+     * @param hashkey
+     * @param steps
+     * @param strength
+     */
+    private static void utilInsertMutiple(String name, Board board, long hashkey, Point[] steps, double strength){
+        //插入模式
+        lib.put(hashkey, new OpenPattern(name, null, hashkey, steps, strength));
+
+        //插入等效模式
+        steps = utilRotate90(steps);
+        hashkey = Zobrist.getRebuildedZobrist(steps).getCurrentCode();
+        lib.put(hashkey, new OpenPattern(name, null, hashkey, steps, strength));
+
+        steps = utilRotate90(steps);
+        hashkey = Zobrist.getRebuildedZobrist(steps).getCurrentCode();
+        lib.put(hashkey, new OpenPattern(name, null, hashkey, steps, strength));
+
+        steps = utilRotate90(steps);
+        hashkey = Zobrist.getRebuildedZobrist(steps).getCurrentCode();
+        lib.put(hashkey, new OpenPattern(name, null, hashkey, steps, strength));
+
+        steps = utilFlipViceDiagonal(steps);
+        hashkey = Zobrist.getRebuildedZobrist(steps).getCurrentCode();
+        lib.put(hashkey, new OpenPattern(name, null, hashkey, steps, strength));
+
+        steps = utilRotate90(steps);
+        hashkey = Zobrist.getRebuildedZobrist(steps).getCurrentCode();
+        lib.put(hashkey, new OpenPattern(name, null, hashkey, steps, strength));
+
+        steps = utilRotate90(steps);
+        hashkey = Zobrist.getRebuildedZobrist(steps).getCurrentCode();
+        lib.put(hashkey, new OpenPattern(name, null, hashkey, steps, strength));
+
+        steps = utilRotate90(steps);
+        hashkey = Zobrist.getRebuildedZobrist(steps).getCurrentCode();
+        lib.put(hashkey, new OpenPattern(name, null, hashkey, steps, strength));
+    }
+
+    private static Point[] utilFlipViceDiagonal(Point[] steps){
+        Point[] result = new Point[steps.length];
+        for (int i = 0; i < result.length; i++) result[i] = new Point();
+        for (int i = 0; i < steps.length; i++){
+            result[i].x = steps[i].y;
+            result[i].y = steps[i].x;
         }
+        return result;
+    }
+
+    private static Point[] utilRotate90(Point[] steps){
+        Point[] result = new Point[steps.length];
+        for (int i = 0; i < result.length; i++) result[i] = new Point();
+        for (int i = 0; i < steps.length; i++){
+            result[i].x = -steps[i].y + 14;
+            result[i].y = steps[i].x;
+        }
+        return result;
+    }
+}
+
+class OpenPattern{
+    public String name;             //局面名称
+//    public Board board;             //棋盘局面
+    public long hashkey;            //哈希值
+    public Point[] steps;            //三步的步法
+    public double strength;          //强度 0 ~ 1，越小对黑越有利
+    OpenPattern(String name, Board board, long hashkey, Point[] steps, double strength){
+        this.name = name;
+//        if (board != null)
+//            this.board = new Board(board);
+        this.hashkey = hashkey;
+        this.steps = new Point[3];
+        this.steps[0] = new Point(steps[0]);
+        this.steps[1] = new Point(steps[1]);
+        this.steps[2] = new Point(steps[2]);
+        this.strength = strength;
     }
 }
